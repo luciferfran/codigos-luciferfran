3bee2547e3971f0e0262593e1aeb14c123f7b255
luciferfran <luciferfran@10fa9776-c713-11dd-8ca8-e3ff79f713b6>
2011-02-21 18:17:27 +0000

    Practica01

---
+++ trunk/C/Practica01/Practica01/Practica01.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SEG_POR_MINUTO 60
+#define SEG_POR_HORA 3600
+#define SEG_POR_DIA 86400
+
+int main()
+{
+	int seg;
+	float min, horas, dias;
+
+	printf ( "Este programa le permite pasar de segundos a minutos,\n" );
+	printf ( "horas o dias\n\n" );
+	printf ( "Escriba cuanntos segundos quiere convertir y pulse ENTER: " );
+	scanf ( "%d", &seg );
+
+	if ( seg < 0 )
+	{
+		printf ( "Error! El numero de segundo debe ser positivo.\n" );
+		printf ( "Saliendo del programa.\n" );
+	}
+	else
+	{
+		min = (float) seg / SEG_POR_MINUTO;
+		horas = (float) seg / SEG_POR_HORA;
+		dias = (float) seg / SEG_POR_DIA;
+
+		printf ( "%d segundos equivalen a %.3f minutos.\n", seg, min );
+		printf ( "%d segundos equivalen a %.3f horas.\n", seg, horas );
+		printf ( "%d segundos equivalen a %.3f dias.\n", seg, dias );
+	}
+
+	system ( "PAUSE" );
+	return 0;
+}
